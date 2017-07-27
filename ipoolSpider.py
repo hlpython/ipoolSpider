b0d67fba3067896e41507323e33567f24cc476ad
Zoctan <752481828@qq.com>
2017-07-26 23:51:22 -0400

    Add files via upload
    fix bug

--- ipoolSpider.py
+++ ipoolSpider.py
@@ -1,61 +1,57 @@
 #!/usr/bin/python3
 # coding=utf-8
-import requests
-import threading
-import codecs
-import json
-import time
+import requests, threading, codecs, time
 from lxml import etree
 
 
 class GetIP(threading.Thread):
     def __init__(self, url, which):
         threading.Thread.__init__(self)
+        self.daemon = True
         self.url = url
         self.which = which
 
     def get_ip(self):
-        time.sleep(1)
-        headers = {
-            'Connection': 'keep-alive',
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'
-        }
+        headers = {'Connection': 'keep-alive',
+                   'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'}
         response = requests.get(self.url, headers=headers, timeout=5)
-        response.encoding = 'utf-8'
         html = etree.HTML(response.text)
-        print(self.url + ' ' + str(response.status_code))
-        x = []
+        print('[' + str(response.status_code) + ']' + self.url)
         if self.which is 'xici':
             for tr in html.xpath('//tr[contains(@class, *)]'):
-                x.append({'ip': tr.xpath('./td/text()')[0],
-                          'port': tr.xpath('./td/text()')[1],
-                          'anonymity': tr.xpath('./td/text()')[4],
-                          'type': tr.xpath('./td/text()')[5]})
-            values['xici'] = x
-
+                allIP.append([tr.xpath('./td/text()')[0], tr.xpath('./td/text()')[1]])
         else:
             for tr in html.xpath('//tbody/tr'):
-                x.append({'ip': tr.xpath('./td[contains(@data-title, "IP")]/text()')[0],
-                          'port': tr.xpath('./td[contains(@data-title, "PORT")]/text()')[0],
-                          'anonymity': tr.xpath('./td[contains(@data-title, "匿名度")]/text()')[0],
-                          'type': tr.xpath('./td[contains(@data-title, "类型")]/text()')[0]})
-            values['kuai'] = x
+                allIP.append(tr.xpath('./td[contains(@data-title, "IP")]/text()')
+                             + tr.xpath('./td[contains(@data-title, "PORT")]/text()'))
 
     def run(self):
-        if threadLock.acquire():
-            self.get_ip()
-            threadLock.release()
+        self.get_ip()
 
 
-def write_json():
-    file = codecs.open('ipool.json', 'wb', encoding='utf-8')
-    line = json.dumps(values, ensure_ascii=False)
-    file.write(line)
-    file.close()
+class CheackIp(threading.Thread):
+    def __init__(self, ipList):
+        threading.Thread.__init__(self)
+        self.daemon = True
+        self.ipList = ipList
+
+    def check_ip(self):
+        for ip in self.ipList:
+            proxy = {'http': ip[0] + ':' + ip[1], 'https': ip[0] + ':' + ip[1]}
+            try:
+                response = requests.get('http://ip.chinaz.com/getip.aspx', proxies=proxy, timeout=5)
+                if response.status_code is 200:
+                    print(ip[0] + ':' + ip[1])
+                    usefulIP.append(ip[0] + ':' + ip[1])
+            except Exception as e:
+                pass
+
+    def run(self):
+        self.check_ip()
 
 
-def run_threads():
-    for i in range(1, 4):
+def run_spider_threads():
+    for i in range(1, 5):
         xici.append('http://www.xicidaili.com/nn/%d' % i)
         kuai.append('http://www.kuaidaili.com/free/inha/%d/' % i)
     for i in range(len(kuai)):
@@ -63,17 +59,40 @@ def run_threads():
         threads.append(GetIP(kuai[i], 'kuai'))
     for i in range(len(threads)):
         threads[i].start()
-    print('[*]Start active threads: %s' % threading.activeCount())
+        time.sleep(1.5)
     for i in range(len(threads)):
         threads[i].join()
-    print('[*]End active threads: %s' % threading.activeCount())
+
+
+def run_check_threads():
+    print('[!]Total crawling %d ip' % len(allIP))
+    x = int(len(allIP)/25)
+    for i in range(25):
+        threads.append(CheackIp(allIP[x*i:x*(i+1)]))
+    for i in range(len(threads)):
+        threads[i].start()
+    print('[*]Start threads: %s' % threading.activeCount())
+    for i in range(len(threads)):
+        threads[i].join()
+    print('[*]End threads: %s\n' % threading.activeCount())     
+
+
+def write():
+    file = codecs.open('ipool.txt', 'wb', encoding='utf-8')
+    for i in usefulIP:
+        file.write(i)
+        file.write('\n')
+    file.close()
+    print('[!]These ip had already stored in ipool.txt')
 
 
 if __name__ == '__main__':
     xici = []
     kuai = []
-    values = {}
+    allIP = []
+    usefulIP = []
     threads = []
-    threadLock = threading.Lock()
-    run_threads()
-    write_json()
+    run_spider_threads()
+    threads = []
+    run_check_threads()
+    write()
